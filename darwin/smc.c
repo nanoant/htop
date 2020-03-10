02993e051f9dad0c647ed2676b437f28af4cd145
Adam Strzelecki <ono@java.pl>
2020-03-10 22:54:56 +0100

    Update darwin smc code to support flt sensor
    Should print now RPM on newer systems.

--- darwin/smc.c
+++ darwin/smc.c
@@ -1,10 +1,36 @@
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <IOKit/IOKitLib.h>
 
+#include "smc.h"
+
+/*
+ * Apple System Management Control (SMC) Tool
+ * Copyright (C) 2006 devnull
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 /*{
 
 #include <IOKit/IOTypes.h>
 
 #define OP_NONE               0
-#define OP_LIST               1 
+#define OP_LIST               1
 #define OP_READ               2
 #define OP_READ_FAN           3
 #define OP_WRITE              4
@@ -19,6 +45,7 @@
 #define SMC_CMD_READ_VERS     12
 
 #define DATATYPE_FPE2         "fpe2"
+#define DATATYPE_FLT          "flt "
 #define DATATYPE_UINT8        "ui8 "
 #define DATATYPE_UINT16       "ui16"
 #define DATATYPE_UINT32       "ui32"
@@ -33,46 +60,46 @@ enum {
  kSMCSuccess = 0,
  kSMCError = 1
 };
- 
+
 enum {
  kSMCUserClientOpen  = 0,
  kSMCUserClientClose = 1,
- kSMCHandleYPCEvent  = 2, 
-      kSMCReadKey         = 5,
+ kSMCHandleYPCEvent  = 2,
+ kSMCReadKey         = 5,
  kSMCWriteKey        = 6,
  kSMCGetKeyCount     = 7,
  kSMCGetKeyFromIndex = 8,
  kSMCGetKeyInfo      = 9
 };
- 
-typedef struct SMCVersion 
+
+typedef struct SMCVersion
 {
     unsigned char    major;
     unsigned char    minor;
     unsigned char    build;
     unsigned char    reserved;
     unsigned short   release;
-     
+
 } SMCVersion;
- 
-typedef struct SMCPLimitData 
+
+typedef struct SMCPLimitData
 {
     uint16_t    version;
     uint16_t    length;
     uint32_t    cpuPLimit;
     uint32_t    gpuPLimit;
     uint32_t    memPLimit;
-     
+
 } SMCPLimitData;
- 
-typedef struct SMCKeyInfoData 
+
+typedef struct SMCKeyInfoData
 {
     IOByteCount         dataSize;
     uint32_t            dataType;
     uint8_t             dataAttributes;
-     
+
 } SMCKeyInfoData;
- 
+
 typedef struct {
     uint32_t            key;
     SMCVersion          vers;
@@ -81,193 +108,183 @@ typedef struct {
     uint8_t             result;
     uint8_t             status;
     uint8_t             data8;
-    uint32_t            data32;    
+    uint32_t            data32;
     uint8_t             bytes[32];
 }  SMCParamStruct;
 
 }*/
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <IOKit/IOKitLib.h>
-
-
-#include "smc.h"
 
 static io_connect_t conn = 0;
 
-uint32_t
-key_to_int(const char *str, int size)
-{
+static uint32_t key_to_int(const char *str, int size) {
     uint32_t total = 0;
     int c;
-     
+
     for (c = 0; c < size; c++) {
         total += str[c] << (size - 1 - c) * 8;
     }
     return total;
 }
 
-float _strtof(uint8_t *str, int size, int e)
-{
-    float total = 0;
-    int i;
-
-    for (i = 0; i < size; i++)
-    {
+static float _strtof(uint8_t *str, int size, int e) {
+    int total = 0;
+    // Add up bits to left of fractional bits
+    for (int i = 0; i < size; i++) {
         if (i == (size - 1))
-           total += (str[i] & 0xff) >> e;
+            total += (str[i] & 0xff) >> e;
         else
-           total += str[i] << (size - 1 - i) * (8 - e);
+            total += (str[i] & 0xff) << (size - 1 - i) * (8 - e);
     }
-
-    return total;
+    // Mask fractional bits and divide
+    return total + (str[size - 1] & (1 << e) - 1) / (float)(1 << e);
 }
 
-int SMCOpen(void)
-{
+static int SMCOpen(void) {
     IOReturn       result;
     io_service_t   device;
 
     device = IOServiceGetMatchingService( kIOMasterPortDefault, IOServiceMatching("AppleSMC"));
     if (IO_OBJECT_NULL == device)
         return -1;
-     
+
     result = IOServiceOpen(device, mach_task_self(), 1, &conn);
     IOObjectRelease(device);
     return 0;
 }
 
-io_connect_t SMCClose(void)
-{
+static io_connect_t SMCClose(void) {
     io_connect_t result;
 
     if (conn != 0) {
         result = IOServiceClose(conn);
-        if (result == 0) 
+        if (result == 0)
             conn = 0;
         return result;
     }
     return -1;
 }
 
-IOReturn
-SMCCall(int index,   SMCParamStruct* input,  SMCParamStruct* output)
-{
+static IOReturn SMCCall(int index, SMCParamStruct* input, SMCParamStruct* output) {
     IOReturn result;
 
     if (conn == 0 && (result = SMCOpen()) != 0)
         return result;
 
     result = IOConnectCallMethod(
-                                          conn, 
-                                          kSMCUserClientOpen, 
-                                          NULL, 
-                                          0, 
-                                          NULL, 
-                                          0, 
-                                          NULL, 
-                                          NULL, 
-                                          NULL, 
+                                          conn,
+                                          kSMCUserClientOpen,
+                                          NULL,
+                                          0,
+                                          NULL,
+                                          0,
+                                          NULL,
+                                          NULL,
+                                          NULL,
                                           NULL);
     if (kIOReturnSuccess != result)
         return result;
-     
+
     size_t outSize = sizeof(SMCParamStruct);
     result = IOConnectCallStructMethod(
-                                       conn, 
-                                       index, 
-                                       input, 
-                                       sizeof(SMCParamStruct), 
-                                       output, 
+                                       conn,
+                                       index,
+                                       input,
+                                       sizeof(SMCParamStruct),
+                                       output,
                                        &outSize);
     IOConnectCallMethod(
-                        conn, 
-                        kSMCUserClientClose, 
-                        NULL, 
-                        0, 
-                        NULL, 
-                        0, 
-                        NULL, 
-                        NULL, 
-                        NULL, 
+                        conn,
+                        kSMCUserClientClose,
+                        NULL,
+                        0,
+                        NULL,
+                        0,
+                        NULL,
+                        NULL,
+                        NULL,
                         NULL);
     return result;
 }
 
-IOReturn
-SMCReadKey(uint32_t     key, 
-         uint8_t*     out, 
-         uint8_t*     size)
-{
+static IOReturn SMCReadKey(uint32_t key, uint8_t* out, uint8_t* size, uint32_t* dataType) {
     SMCParamStruct input;
     SMCParamStruct output;
-     
+
     bzero(out,     *size);
     bzero(&input,  sizeof(SMCParamStruct));
     bzero(&output, sizeof(SMCParamStruct));
-     
+
     // Advise the SMC to get meta-info about a key
     input.data8 = kSMCGetKeyInfo;
     input.key   = key;
 
-    IOReturn result = SMCCall( kSMCHandleYPCEvent, 
-                               &input, 
+    IOReturn result = SMCCall( kSMCHandleYPCEvent,
+                               &input,
                                &output);
     if (kSMCSuccess != result)
         return result;
-     
+
     // Get the value for the key
     input.data8 = kSMCReadKey;
     input.key   = key;
     input.keyInfo.dataSize = output.keyInfo.dataSize;
-     
+
+    *dataType = output.keyInfo.dataType;
+
     bzero(&output, sizeof(SMCParamStruct));
-     
+
     result = SMCCall(kSMCHandleYPCEvent, &input, &output);
     if (kSMCSuccess != result)
         return result;
-     
+
     // Copy the data to out
     if (*size > input.keyInfo.dataSize)
         *size = input.keyInfo.dataSize;
-     
+
     memcpy(out, output.bytes, *size);
     return kSMCSuccess;
 }
 
-float
-SMCGetFanRPM(const char *key_text) {
+float SMCGetFanRPM(const char *key_text) {
     uint8_t buffer[256];
     uint8_t size=255;
     float result;
     uint32_t key;
+    uint32_t dataType;
 
     key = key_to_int(key_text, 4);
-    result = SMCReadKey(key, buffer, &size);
-    if (result != 0) 
+    result = SMCReadKey(key, buffer, &size, &dataType);
+    if (result != 0)
         return -1;
-    
-    result = _strtof(buffer, (int)size, 2);
+
+    if (dataType == key_to_int(DATATYPE_FPE2, 4)) {
+        result = _strtof(buffer, (int)size, 2);
+    } else if (dataType == key_to_int(DATATYPE_FLT, 4)) {
+        result = *(float*)buffer;
+    } else {
+        return -1;
+    }
     SMCClose();
     return result;
 }
 
-double
-SMCGetTemperature(const char *key_text) {
+double SMCGetTemperature(const char *key_text) {
     uint8_t buffer[256];
     uint8_t size=255;
     double result;
     uint32_t key;
+    uint32_t dataType;
 
     key = key_to_int(key_text, 4);
-    result = SMCReadKey(key, buffer, &size);
-    if (result != 0) 
+    result = SMCReadKey(key, buffer, &size, &dataType);
+    if (result != 0)
         return -10000.0;
+    if (dataType == key_to_int(DATATYPE_SP78, 4)) {
+        result = ((uint16_t)buffer[1] + ((uint16_t)buffer[0] << 8)) / 256.0;
+    } else {
+        result = -1;
+    }
 
-    result = ((uint16_t)buffer[1] + ((uint16_t)buffer[0] << 8)) / 256.0;
     SMCClose();
     return result;
 }
-
